8ab21985e95bb7e9d67d2baa156c30997942650b
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-10-19 22:41:31 +0700

    Add URL validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-import './scss/styles.scss';
+import { object, string } from 'yup';
+import onChange from 'on-change';
 
-console.log('Hello, world!');
+import './scss/styles.scss';
+import render from './view.js';
+
+const app = () => {
+  const state = {
+    rssForm: {
+      state: '',
+      feedback: '',
+    },
+    addedUrls: [],
+  };
+
+  const validateUrl = (url) => {
+    const schema = object({
+      url: string()
+        .url('Link must be a valid URL')
+        .required()
+        .notOneOf(state.addedUrls, 'RSS already exists'),
+    });
+    return schema.validate({ url });
+  };
+
+  const rssForm = document.querySelector('form.rss-form');
+
+  const watchedState = onChange(state, render);
+
+  // https://aljazeera.com/xml/rss/all.xml
+
+  rssForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(rssForm);
+    const url = data.get('url');
+
+    validateUrl(url)
+      .then(() => {
+        watchedState.rssForm.state = 'valid';
+        watchedState.rssForm.feedback = 'RSS was successfully loaded';
+        watchedState.addedUrls.push(url);
+      })
+      .catch((err) => {
+        [watchedState.rssForm.feedback] = err.errors;
+        watchedState.rssForm.state = 'invalid';
+      });
+  });
+};
+
+app();
---
+++ src/view.js
@@ -0,0 +1,28 @@
+const render = (path, value) => {
+  const rssForm = document.querySelector('form.rss-form');
+  if (path === 'rssForm.state') {
+    const urlInput = rssForm.querySelector('input[aria-label="url"]');
+    const feedbackElement = document.querySelector('.feedback');
+    switch (value) {
+      case 'invalid':
+        urlInput.classList.add('is-invalid');
+        feedbackElement.classList.remove('text-success');
+        feedbackElement.classList.add('text-danger');
+        break;
+      case 'valid':
+        urlInput.classList.remove('is-invalid');
+        feedbackElement.classList.remove('text-danger');
+        feedbackElement.classList.add('text-success');
+        rssForm.reset();
+        urlInput.focus();
+        break;
+      default:
+        throw new Error(`Invalid state: ${value}`);
+    }
+  } else if (path === 'rssForm.feedback') {
+    const feedbackElement = document.querySelector('.feedback');
+    feedbackElement.textContent = value;
+  }
+};
+
+export default render;
